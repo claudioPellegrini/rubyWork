17d064811cfaffa090757294ac2d3b470ca43f73
claudioPellegrini <ladeclaudio@gmail.com>
2016-11-16 15:55:57 -0200

    formularios

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,7 @@
+
 class Account < ApplicationRecord
+  validates :name, presence: true
+  validates :kind, inclusion: { in: %w(bank cash credit-card) }
+  validates :balance, numericality: true
+  validates :currency, inclusion: { in: %w(USD UYU ARS) }
 end
